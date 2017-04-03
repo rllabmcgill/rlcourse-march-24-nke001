f366253e1eb758b67738a7d803cb76ff95800227
Nan Rosemary Ke <rosemary.ke@west.cmu.edu>
2017-04-03 00:24:56 -0400

    analysis

--- code/analysis_compare.py
+++ code/analysis_compare.py
@@ -4,17 +4,19 @@ import matplotlib.pyplot as plt
 import os
 import sys
 
-file_name = sys.argv[1]
+k5_file_name = sys.argv[1]
+k11_file_name = sys.argv[2]
 
 epoch = 0
 rewards_epoch = []
 step_episodes = [] 
 last_step = 10000
-avg_epoch_rewards = []
+k5_avg_epoch_rewards = []
+k11_avg_epoch_rewards = []
 step_epoch = 10000
 
 def compute_rewards():
-    global epoch, step_epoch, rewards_epoch, step_episodes, last_step, avg_epoch_rewards
+    global epoch, step_epoch, rewards_epoch, step_episodes, last_step, k5_avg_epoch_rewards, k11_avg_epoch_rewards
     if epoch == 0:
         epoch += 1
         return 
@@ -25,12 +27,16 @@ def compute_rewards():
     avg_reward = total/ step_epoch
     rewards_epoch = []
     step_episodes = []
-    avg_epoch_rewards.append(avg_reward)
+    if k5:
+        k5_avg_epoch_rewards.append(avg_reward)
+    else:
+        k11_avg_epoch_rewards.append(avg_reward)
     print 'epoch ', epoch, ' avg reward epoch ', avg_reward 
     epoch += 1
     last_step = 10000
 
-with open(file_name, 'rb') as in_file:
+k5 = True
+with open(k5_file_name, 'rb') as in_file:
     lines = in_file.readlines()
 
 for line in lines:
@@ -46,11 +52,33 @@ for line in lines:
     if 'reward' in line:
         rewards_epoch.append(float(line.split()[-1]))
 
+k5_epoch = epoch
+k5 = False
+print 'k=11 =========================='
+epoch = 0
+with open(k11_file_name, 'rb') as in_file:
+    lines = in_file.readlines()
+
+for line in lines:
+    if 'epoch' in line:
+        line_split = line.split()
+        current_epoch = int(line_split[2])
+        if current_epoch > epoch:
+            compute_rewards()
+        else:
+            step = int(line_split[-1])
+            step_episodes.append(last_step - step)
+            last_step = step
+    if 'reward' in line:
+        rewards_epoch.append(float(line.split()[-1]))
+
+
 import ipdb; ipdb.set_trace()
-plt.plot(range(1, epoch), avg_epoch_rewards, color='blue')
+plt.plot(range(1, k5_epoch), k5_avg_epoch_rewards)
+plt.plot(range(1, epoch), k11_avg_epoch_rewards)
 plt.ylabel('average reward per 10,000 steps')
 plt.xlabel('epoch (10,000 steps) number')
-plt.savefig(file_name+'_plot.jpg')
+plt.savefig('space_k5_k11_comparison_plot.jpg')
 
 
 
